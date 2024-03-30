5f71aceba9fabe390fda8f04d7ef4cb186b9c367
Allison Lopes da Silva Bosco <72554824+lopes7890@users.noreply.github.com>
2024-03-30 10:33:23 -0300

    Delete exercicio 2 directory

--- exercicio 2/script.js
+++
@@ -1,9 +0,0 @@
-/* 2) Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas para ela:
-Ex:
-Qual é o seu nome? João da Silva
-Olá João da Silva, é um prazer te conhecer! */
-
-let nome = prompt('qual seu nome: ');
-
-console.log(`ola ${nome}, prazer te conhecer`);
-
