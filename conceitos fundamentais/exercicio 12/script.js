6b08b9174cd4d13c37fc549b40e1f48488a9e295
Allison Lopes da Silva Bosco <72554824+lopes7890@users.noreply.github.com>
2024-04-02 16:04:34 -0300

    basic

---
+++ conceitos fundamentais/exercicio 12/script.js
@@ -0,0 +1,13 @@
+/* A função Math.pow() exibe a potência de um número.
+Teste a função com console.log() e as seguintes bases: 2, 3, 18 e o
+expoente deve ser 2.
+
+*/
+
+let NumeroDois = Math.pow(2,2);
+let NumeroTres = Math.pow(3,2);
+let NumeroDezoito = Math.pow(18,2);
+
+console.log(NumeroDezoito);
+console.log(NumeroDois);
+console.log(NumeroTres);
