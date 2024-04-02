c51c4bc16efda8666cc9fc07bf3cd33fa9d757e2
Allison Lopes da Silva Bosco <72554824+lopes7890@users.noreply.github.com>
2024-04-02 15:58:33 -0300

    basic

---
+++ conceitos fundamentais/exercicio 11/script.js
@@ -0,0 +1,12 @@
+/* Armazene valores em variáveis com cada um dos tipos de 
+dados vistos;]
+String, Number e Boolean
+*/
+
+let texto = 'ola mundo'
+let numero = 10
+let VerdadeiroFalso = 5>3
+
+console.log(typeof texto);
+console.log(typeof numero);
+console.log(typeof VerdadeiroFalso);
