115d09bf16fed4f1fb1ea04ccd214664e23ea536
Allison Lopes da Silva Bosco <72554824+lopes7890@users.noreply.github.com>
2024-04-04 22:19:54 -0300

    objetos e arrays

---
+++ objetos e arrays/exercicio 2/script.js
@@ -0,0 +1,9 @@
+//crie dois arrays um com dois elementos e um com 4 e imprima o tamanho
+// de cada um deles.
+
+let NumerosArray1 = [1, 2];
+
+let NumerosArray2 = [1, 2, 3, 4];
+
+console.log(NumerosArray1.length);
+console.log(NumerosArray2.length);
