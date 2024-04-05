40f89421c8f59224b410603dca4a48ed1ed108b7
Allison Lopes da Silva Bosco <72554824+lopes7890@users.noreply.github.com>
2024-04-04 22:21:46 -0300

    objetos e arrays

---
+++ objetos e arrays/exercicio 7/script.js
@@ -0,0 +1,8 @@
+// crie um array com 5 elementos;
+// faça uma iteração entre todos eles e imprima no console o valor;
+
+let nums = [1, 3, 4, 6, 7, 8, 9];
+
+nums.forEach(n => {
+    console.log(n);
+});
---
+++ objetos e arrays/exercicio 8/script.js
@@ -0,0 +1,13 @@
+// crie um JSON com 3 propriedades;
+// atribua ele a uma variavel
+// acesse as propriedades imprimindo no console
+
+const obj = {
+    "nome": "allison",
+    "idade": 16,
+    "namorada": "suimdara"
+}
+
+console.log(obj.nome);
+console.log(obj.idade);
+console.log(obj.namorada);
