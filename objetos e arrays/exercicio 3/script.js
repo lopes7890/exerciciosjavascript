0c87ed80069b61e999e56ba8b6814fa227e3e9fc
Allison Lopes da Silva Bosco <72554824+lopes7890@users.noreply.github.com>
2024-04-04 22:20:31 -0300

    objetos e arrays

---
+++ objetos e arrays/exercicio 3/script.js
@@ -0,0 +1,13 @@
+// crie um objeto onibus com as propriedades rodas, passageiros e portas.
+
+const onibus = {
+
+    rodas: 8,
+    passageiros: 40,
+    portas: 2
+
+};
+
+console.log('rodas:' + onibus.rodas);
+console.log('limite de passageiros: ' + onibus.passageiros);
+console.log('portas: ' + onibus.portas);
