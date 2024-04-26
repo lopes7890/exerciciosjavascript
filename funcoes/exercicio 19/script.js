ba449c1408a86cfdbffeadddad114439c0859a38
Allison Lopes da Silva Bosco <72554824+lopes7890@users.noreply.github.com>
2024-04-26 15:47:19 -0300

    funcoes

--- funcoes/exercicio 19/script.js
+++ funcoes/exercicio 19/script.js
@@ -2,9 +2,16 @@
 em uma string fornecida pelo usuário.
 Por exemplo, o usuário informa a string “Beterraba”, 
 e a função retorna o número 4 (há 4 vogais nessa palavra).*/
-function vogal(palavra){
-        
-
+function vogal(palavra){     
+        let formatar = palavra.toLowerCase()
+        let letras = formatar.match(/[a-u]/g);
+        let contVogais = 0
+        for(let i = 0; i < letras.length; i++){
+                if(letras[i] == 'a' || letras[i] == 'e' || letras[i] == 'i' || letras[i] == 'o' || letras[i] == 'u'){
+                    contVogais++
+                }
+        }
+        console.log(contVogais);
 }
 
 let palavra = prompt('digite uma palavra: ');
