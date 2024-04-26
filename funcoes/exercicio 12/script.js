a86e4b4fc2eab271e3e02a685f251aa5b07d8c43
Allison Lopes da Silva Bosco <72554824+lopes7890@users.noreply.github.com>
2024-04-26 15:41:10 -0300

    funcoes

--- funcoes/exercicio 12/script.js
+++ funcoes/exercicio 12/script.js
@@ -1,11 +1,14 @@
 /*Escreva uma função que calcule e retorne o 
 fatorial de um número inteiro fornecido pelo usuário.*/
-function fatorial(numero){
-    let cont = numero
+function fatorial(num){
+    if(num == 1){
+        return 1
+    }
 
+    return num * fatorial(num - 1);
+    
 }
-
 
 let numero = Number(prompt('digite um numero: '));
 
-fatorial(numero);
+console.log(fatorial(numero));
